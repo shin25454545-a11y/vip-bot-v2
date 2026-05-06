e05c17639caad5f4299c1cd266f2ebf87eae1959
shin25454545-a11y <shin25454545@gmail.com>
2026-05-06 18:43:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Bot is alive!"
+def run():
+    app.run(host='0.0.0.0', port=8080)
+Thread(target=run).start()
+
+
+
 import discord
 from discord.ext import commands
 import datetime
